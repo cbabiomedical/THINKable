995e36abd64f68c29aeb416e40cf68565125ca09
cbabiomedical <90172658+cbabiomedical@users.noreply.github.com>
2021-09-13 14:08:06 +0530

    Forgot Password

--- app/src/main/java/com/example/thinkableproject/ChangePassword.java
+++ app/src/main/java/com/example/thinkableproject/ChangePassword.java
@@ -3,8 +3,12 @@ package com.example.thinkableproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class ChangePassword extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/thinkableproject/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/thinkableproject/ForgetPasswordActivity.java
@@ -1,34 +1,83 @@
 package com.example.thinkableproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
+    private EditText emailEditText;
+    private Button resetPasswordButton;
+    private ProgressBar progressBar;
 
-    EditText newPassword;
-    EditText confirmPassword;
-    Button resetPassword;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
 
-        newPassword=findViewById(R.id.newPassword);
-        confirmPassword=findViewById(R.id.confirmPassword);
-        resetPassword=findViewById(R.id.resetPassword);
+        emailEditText = (EditText) findViewById(R.id.email);
+        resetPasswordButton = (Button) findViewById(R.id.resetPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-        resetPassword.setOnClickListener(new View.OnClickListener() {
+        auth = FirebaseAuth.getInstance();
+
+        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),SignInActivity.class);
-                startActivity(intent);
+                resetPassword();
             }
         });
+    }
+
+    private void resetPassword() {
+        String email = emailEditText.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Email is Required");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Please provide valid email!");
+            emailEditText.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ForgetPasswordActivity.this, "Check your email to reset your password & Log in again to continue!", Toast.LENGTH_LONG)
+                            .show();
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            startActivity(new Intent(ForgetPasswordActivity.this, SignInActivity.class));
+                        }
+                    }, 8000);
+                } else {
+                    Toast.makeText(ForgetPasswordActivity.this, "Something went wrong! Try again", Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+
     }
 }
