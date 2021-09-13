a488a887ca52c9337601fac223242c7806aff59d
cbabiomedical <90172658+cbabiomedical@users.noreply.github.com>
2021-09-13 17:05:24 +0530

    Change Password

--- app/src/main/java/com/example/thinkableproject/ChangePassword.java
+++ app/src/main/java/com/example/thinkableproject/ChangePassword.java
@@ -1,18 +1,82 @@
 package com.example.thinkableproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ChangePassword extends AppCompatActivity {
+    EditText current_password;
+    EditText new_Password;
+    Button resetPassword;
 
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        current_password = (EditText) findViewById(R.id.current_password);
+        new_Password = (EditText) findViewById(R.id.newPassword);
+        resetPassword = (Button) findViewById(R.id.resetPassword);
+
+        resetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String oldPassword = current_password.getText().toString().trim();
+                String newPassword = new_Password.getText().toString().trim();
+                if (TextUtils.isEmpty(oldPassword)){
+                    Toast.makeText(ChangePassword.this, "Enter your current password...", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (newPassword.length()<6){
+                    Toast.makeText(ChangePassword.this, "Password length must atleast 6 characters...", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                updatePassword(oldPassword, newPassword);
+            }
+        });
+    }
+
+    private void updatePassword(String oldPassword, String newPassword){
+        FirebaseUser user = auth.getCurrentUser();
+
+        AuthCredential authCredential = EmailAuthProvider.getCredential(user.getEmail(), oldPassword);
+        user.reauthenticate(authCredential).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                user.updatePassword(newPassword).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(ChangePassword.this, "Password Updated...", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ChangePassword.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ChangePassword.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
