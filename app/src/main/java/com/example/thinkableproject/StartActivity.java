c9c0646fc5f6a0fb48c83a25e943e3aa3fb99613
venushikavitha1234 <venushi.20191028@iit.ac.lk>
2021-09-16 02:17:55 +0530

    Splash activity animation

--- app/src/main/java/com/example/thinkableproject/StartActivity.java
+++ app/src/main/java/com/example/thinkableproject/StartActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
@@ -12,6 +13,8 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
 
     Animation topAnim;
     ImageView image;
@@ -28,10 +31,25 @@ public class StartActivity extends AppCompatActivity {
         image = findViewById(R.id.imageiew);
 
         image.setAnimation(topAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(StartActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },SPLASH_SCREEN);
+
+
     }
 
     public void startSignup(View view) {
         Intent intent=new Intent(StartActivity.this, SignInActivity.class);
         startActivity(intent);
     }
+
+
 }
