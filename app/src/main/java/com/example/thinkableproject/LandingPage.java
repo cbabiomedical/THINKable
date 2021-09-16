d2cc054e728caf4b3f58023a0c3287dcf350a108
venushikavitha1234 <venushi.20191028@iit.ac.lk>
2021-09-16 15:36:42 +0530

    Google SignIn

--- app/src/main/java/com/example/thinkableproject/LandingPage.java
+++ app/src/main/java/com/example/thinkableproject/LandingPage.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -41,6 +44,11 @@ public class LandingPage extends AppCompatActivity {
             }
         });
 
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount != null){
+            Toast.makeText(this, "Sign Up Successfully", Toast.LENGTH_SHORT).show();
+        }
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,4 +58,10 @@ public class LandingPage extends AppCompatActivity {
         });
     }
 
+
+    public void logoutfrmGgl(View view) {
+        FirebaseAuth.getInstance().signOut();
+        Intent intentSignotGgl=new Intent(getApplicationContext(),SignInActivity.class);
+        startActivity(intentSignotGgl);
+    }
 }
--- app/src/main/java/com/example/thinkableproject/RegisterActivity.java
+++ app/src/main/java/com/example/thinkableproject/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.example.thinkableproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -15,16 +16,26 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -33,6 +44,7 @@ import java.util.Calendar;
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText username, emailAddress, userPassword, re_enterPassword;
+    private ImageView imageViewggl;
     private DatePickerDialog datePickerDialog;
     private TextView signIn;
     private Button dateButton;
@@ -45,7 +57,22 @@ public class RegisterActivity extends AppCompatActivity {
     ProgressBar progressBar;
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
+    private GoogleSignInClient mGoogleSignInClient;
 
+    private final static int RC_SIGN_IN=123;
+    private FirebaseAuth mAuthggl;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = mAuthggl.getCurrentUser();
+        if(user!=null){
+            Intent intentg = new Intent (getApplicationContext(),SignInActivity.class);
+            startActivity(intentg);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +92,20 @@ public class RegisterActivity extends AppCompatActivity {
         initDatePicker();
 
         dateButton.setText(getTodayDate());
+
+        mAuthggl = FirebaseAuth.getInstance();
+
+
+
+
+        createRequest();
+
+        findViewById(R.id.imageViewggl).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                signInggl();
+            }
+        });
 
         String[] items = new String[]{"Teacher", "Manager", "Doctor", "Engineer", "MicroBiologist"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
@@ -99,6 +140,67 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void createRequest(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+    }
+
+    private void signInggl() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            firebaseAuthWithGoogle(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(),null);
+        mAuthggl.signInWithCredential(credential)
+                .addOnCompleteListener(this,new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete(@NonNull Task <AuthResult> task){
+                        if (task.isSuccessful()){
+                            FirebaseUser user = mAuthggl.getCurrentUser();
+                            Intent intentg = new Intent (getApplicationContext(),SignInActivity.class);
+                            startActivity(intentg);
+
+                        } else{
+                            Toast.makeText(RegisterActivity.this, "Sorry auth failed", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
 
     }
 
@@ -268,9 +370,5 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    public void gotoOTP(View view) {
-        Intent intentotp = new Intent(RegisterActivity.this,EnterPhoneActivity.class);
-        startActivity(intentotp);
 
-    }
 }
