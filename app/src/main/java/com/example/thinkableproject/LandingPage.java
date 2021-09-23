1e93cf0e5c2e15c55eaded3b356144fe955a2040
Subhaka <subhaka99@gmail.com>
2021-09-23 09:31:14 +0530

    Upload text file to firebase

--- app/src/main/java/com/example/thinkableproject/LandingPage.java
+++ app/src/main/java/com/example/thinkableproject/LandingPage.java
@@ -1,16 +1,25 @@
 package com.example.thinkableproject;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,13 +27,40 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class LandingPage extends AppCompatActivity {
+    private static final int PICK_FILE_REQUEST = 234;
     Button changepassword, logout, relaxationLanding, concentrationLanding;
+    Button chooseFile, uploadFile;
+    private Uri filePath;
+    private StorageReference storageReference;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
         concentrationLanding=findViewById(R.id.concentrationDaily);
+        chooseFile=findViewById(R.id.choose);
+        uploadFile=findViewById(R.id.upload);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        Log.d("User", mUser.getUid());
+        chooseFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+
+
+        storageReference= FirebaseStorage.getInstance().getReference().child(mUser.getUid());
+        uploadFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadFileD();
+            }
+        });
         concentrationLanding.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,6 +68,7 @@ public class LandingPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
 //      /  BottomNavigationView navView = findViewById(R.id.nav_view);
         relaxationLanding=findViewById(R.id.relaxationLanding);
         // Passing each menu ID as a set of Ids because each
@@ -72,6 +109,45 @@ relaxationLanding.setOnClickListener(new View.OnClickListener() {
             }
         });
     }
+    private void uploadFileD() {
+
+
+        StorageReference riverRef = storageReference.child("daily.txt");
+        Log.d("UserCheck",mUser.getUid());
+        riverRef.putFile(filePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(LandingPage.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LandingPage.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    private void showFileChooser() {
+        Intent intent=new Intent();
+        intent.setType("text/*");intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"select file"),PICK_FILE_REQUEST);
+    }
+
+
+
+
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode, data);
+        if(requestCode==PICK_FILE_REQUEST&& resultCode==RESULT_OK&& data!=null&& data.getData()!=null) {
+            filePath=data.getData();
+            Log.d("path", String.valueOf(filePath));
+        }
+    }
+
 
 
     public void logoutfrmGgl(View view) {
