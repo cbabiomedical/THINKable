ace178347de63926ddf37856c50cad90c115d3f7
cbabiomedical <90172658+cbabiomedical@users.noreply.github.com>
2021-12-19 18:50:24 +0530

    Concentration Game

--- app/src/main/java/com/example/thinkableproject/sample/JsonPlaceHolder.java
+++ app/src/main/java/com/example/thinkableproject/sample/JsonPlaceHolder.java
@@ -19,9 +19,23 @@ public interface JsonPlaceHolder {
     @POST("eeg_values")
     Call<EEG_Values> createPost(@Body EEG_Values eeg_values);
 
-
-
-
+//    @GET("concentration_values")
+//    Call<List<Concentration_Values>>  getCValues(@Query("name1")String name);
+//
+//    @POST("concentration_values")
+//    Call<Concentration_Values> createPost(@Body Concentration_Values concentration_values);
+//
+//    @GET("relaxation_values")
+//    Call<List<Relaxation_Values>>  getRValues(@Query("name2")String name);
+//
+//    @POST("relaxation_values")
+//    Call<Relaxation_Values> createPost(@Body Relaxation_Values relaxation_values);
+//
+//    @GET("calibration")
+//    Call<List<Relaxation_Values>>  getcalValues(@Query("name2")String name);
+//
+//    @POST("relaxation_values")
+//    Call<Relaxation_Values> createPost(@Body Relaxation_Values relaxation_values);
 
 
 }
