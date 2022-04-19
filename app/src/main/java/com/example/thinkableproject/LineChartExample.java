cec96c77aed448ac552afc79ddda908627760b27
venushikavitha1234 <venushi.20191028@iit.ac.lk>
2022-04-19 10:29:44 +0530

    Update LineChartExample.java

--- app/src/main/java/com/example/thinkableproject/LineChartExample.java
+++ app/src/main/java/com/example/thinkableproject/LineChartExample.java
@@ -6,9 +6,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
+
+import com.example.thinkableproject.sample.JsonPlaceHolder;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -23,8 +27,29 @@ import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.LinkedTreeMap;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LineChartExample extends AppCompatActivity {
     private InterstitialAd mInterstitialAd;
@@ -33,10 +58,13 @@ public class LineChartExample extends AppCompatActivity {
     LineData lineData;
     LineDataSet lineDataSet;
     ArrayList lineEntries;
-//    TextView earnedPoints;
+    //    TextView earnedPoints;
+    JsonPlaceHolder jsonPlaceHolder;
+    int c;
     FirebaseFirestore database;
+    FirebaseUser mUser;
     User user;
-//    TextView total_points;
+    //    TextView total_points;
     int totalPoints;
 
     @Override
@@ -45,6 +73,66 @@ public class LineChartExample extends AppCompatActivity {
         setContentView(R.layout.activity_line_chart_example);
         ok = findViewById(R.id.ok);
         database = FirebaseFirestore.getInstance();
+
+        SharedPreferences shs = getSharedPreferences("prefsTimeRelWH", MODE_APPEND);
+
+        c = shs.getInt("firstStartTimeRelWH", 0);
+
+        Calendar now = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Log.d("WEEK", String.valueOf(now.get(Calendar.WEEK_OF_MONTH)));
+        Log.d("MONTH", String.valueOf(now.get(Calendar.MONTH)));
+        Log.d("YEAR", String.valueOf(now.get(Calendar.YEAR)));
+        Log.d("DAY", String.valueOf(now.get(Calendar.DAY_OF_MONTH)));
+
+        int month = now.get(Calendar.MONTH) + 1;
+        int day = now.get(Calendar.DAY_OF_MONTH) + 1;
+        Format f = new SimpleDateFormat("EEEE");
+        String str = f.format(new Date());
+        Log.d("NinjaDartIndex", String.valueOf(BroadcastReceiver_BTLE_GATT.concentration_indexesND));
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(120000, TimeUnit.SECONDS)
+                .readTimeout(120000, TimeUnit.SECONDS).build();
+
+        if (BroadcastReceiver_BTLE_GATT.concentration_indexesND.size() > 0) {
+
+            Retrofit retrofit = new Retrofit.Builder().baseUrl("http://192.168.8.105:5000/").client(client)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build();
+            jsonPlaceHolder = retrofit.create(JsonPlaceHolder.class);
+            //Post Time to Concentrate and Time stayed Concentrated
+            ArrayList data = new ArrayList((Arrays.asList(0.5, 0.6, 0.7, 0.3, 0.8, 0.55, 0.65, 0.75, 0.45, 0.7, 0.65, 0.55, 0.1, 0.45)));
+            Call<List> callMusRel = jsonPlaceHolder.PostTimeToConcentrate(BroadcastReceiver_BTLE_GATT.concentration_indexesND);
+            callMusRel.enqueue(new Callback<List>() {
+                @Override
+                public void onResponse(Call<List> call, Response<List> response) {
+                    Toast.makeText(getApplicationContext(), "Post Space Successful", Toast.LENGTH_SHORT).show();
+                    Log.d("SUMCONResponseTime DH", String.valueOf(response.code()));
+                    Log.d("SUMCONResTime Message", response.message());
+                    Log.d("SUMCONResTime Body", String.valueOf(response.body()));
+                    List com = response.body();
+                    LinkedTreeMap hashmap = new LinkedTreeMap();
+                    hashmap = (LinkedTreeMap) com.get(0);
+                    Log.d("SUMCONH", String.valueOf(hashmap.get("time_to_concentrate")));
+
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("TimeTo").child("Relaxation").child(mUser.getUid()).child(String.valueOf(now.get(Calendar.YEAR))).child(String.valueOf(month)).child(String.valueOf(now.get(Calendar.WEEK_OF_MONTH))).child(str).child(String.valueOf(c));
+                    databaseReference.setValue(hashmap.get("time_to_concentrate"));
+                    DatabaseReference databaseReferencet = FirebaseDatabase.getInstance().getReference("TimeStayed").child("Relaxation").child(mUser.getUid()).child(String.valueOf(now.get(Calendar.YEAR))).child(String.valueOf(month)).child(String.valueOf(now.get(Calendar.WEEK_OF_MONTH))).child(str).child(String.valueOf(c));
+                    databaseReferencet.setValue(hashmap.get("time concentrated"));
+
+                }
+
+                @Override
+                public void onFailure(Call<List> call, Throwable t) {
+//                                    Toast.makeText(context, "Failed Post", Toast.LENGTH_SHORT).show();
+                    Log.d("ErrorValSpa", String.valueOf(t));
+                }
+            });
+        }
 
         SharedPreferences sharedPreferences = getSharedPreferences("myKey", MODE_PRIVATE);
         int value = sharedPreferences.getInt("value", 0);
